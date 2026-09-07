104563e3fb6975a04ec939ce736a2d30d6da8999
ossamasal2012 <ossamasal2012@gmail.com>
2026-09-07 12:30:12 +0400

    Update customize-android.js

--- scripts/customize-android.js
+++ scripts/customize-android.js
@@ -269,7 +269,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         UpdateManager.resumePendingInstallIfReady(this);
     }
